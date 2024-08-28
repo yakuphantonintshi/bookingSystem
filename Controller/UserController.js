51da97b8d3900151bcbb9f6047bd83231c403294
Yakupha <yakupha58756@gmail.com>
2024-08-28 20:24:40 +0200

    EndpointsFix

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,7 +1,50 @@
+// import express from 'express'
+// import bodyParser from 'body-parser'
+
+
+// import { users } from '../model/index.js'
+
+// const userRouter = express.Router()
+
+// userRouter.use(bodyParser.json())
+
+// userRouter.get('/', (req, res) => {
+//     users.fetchUsers(req, res)
+// })
+
+// userRouter.get('/:id', (req, res) => {
+//     users.fetchUser(req,res)
+// })
+
+// userRouter.post('/register', (req, res) => {
+//     users.registerUser(req,res)
+// })
+
+// userRouter.patch('/user/:id', (req, res) => {
+//     users.updateUser(req,res)
+// })
+
+// userRouter.delete('/user/:id', (req, res) => {
+//     users.deleteUser(req,res)
+// })
+// userRouter.post('/login', (req, res) => {
+//     users.login(req,res)
+// })
+
+// userRouter.get("*", (req, res) => {
+//     res.json({
+//       status: 404,
+//       msg: "Resource not found",
+//     });
+//   });
+
+// export{
+//     express,
+//     userRouter
+// }
+
 import express from 'express'
 import bodyParser from 'body-parser'
-
-
 import { users } from '../model/index.js'
 
 const userRouter = express.Router()
@@ -19,11 +62,11 @@ userRouter.post('/register', (req, res) => {
     users.registerUser(req,res)
 })
 
-userRouter.patch('/user/:id', (req, res) => {
+userRouter.patch('/:id', (req, res) => {
     users.updateUser(req,res)
 })
 
-userRouter.delete('/user/:id', (req, res) => {
+userRouter.delete('/:id', (req, res) => {
     users.deleteUser(req,res)
 })
 userRouter.post('/login', (req, res) => {

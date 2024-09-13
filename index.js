5d141a5c87e92515df9117729527f151877397e0
yakuphantonintshi <yakupha58756@gmail.com>
2024-09-13 17:04:19 +0200

    Update2

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const port = +process.env.PORT || 4000;
 
 
 // Middleware
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Request-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Expose-Headers", "Authorization");
-    next()
-})
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Methods", "*");
+//     res.header("Access-Control-Request-Methods", "*");
+//     res.header("Access-Control-Allow-Headers", "*");
+//     res.header("Access-Control-Expose-Headers", "Authorization");
+//     next()
+// })
 
 app.use(cors({
   origin: '*',
